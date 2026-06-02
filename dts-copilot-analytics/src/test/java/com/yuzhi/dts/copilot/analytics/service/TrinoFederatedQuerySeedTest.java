8e6b7fb06e6174228d1c57b5ee1946007d0c2e53
billy <billy.xiezhimin@gmail.com>
2026-06-02 10:49:31 +0800

    fix:enhancemnt

--- dts-copilot-analytics/src/test/java/com/yuzhi/dts/copilot/analytics/service/TrinoFederatedQuerySeedTest.java
+++ dts-copilot-analytics/src/test/java/com/yuzhi/dts/copilot/analytics/service/TrinoFederatedQuerySeedTest.java
@@ -12,14 +12,21 @@ class TrinoFederatedQuerySeedTest {
             "config/liquibase/changelog/0065_prs_flowerbiz_screen_logical_datasource.xml";
     private static final String FEDERATED_CHANGELOG =
             "config/liquibase/changelog/0066_trino_federated_query_database.xml";
+    private static final String FEDERATED_AGENT_DATASOURCE_CHANGELOG =
+            "config/liquibase/changelog/0067_trino_federated_agent_datasource.xml";
 
     @Test
     void masterShouldIncludeTrinoFederatedSeedAfterLogicalDatasourceRepair() throws Exception {
         String master = readResource("config/liquibase/master.xml");
 
-        assertThat(master).contains(LOGICAL_DATASOURCE_CHANGELOG, FEDERATED_CHANGELOG);
+        assertThat(master).contains(
+                LOGICAL_DATASOURCE_CHANGELOG,
+                FEDERATED_CHANGELOG,
+                FEDERATED_AGENT_DATASOURCE_CHANGELOG);
         assertThat(master.indexOf(LOGICAL_DATASOURCE_CHANGELOG))
                 .isLessThan(master.indexOf(FEDERATED_CHANGELOG));
+        assertThat(master.indexOf(FEDERATED_CHANGELOG))
+                .isLessThan(master.indexOf(FEDERATED_AGENT_DATASOURCE_CHANGELOG));
     }
 
     @Test
@@ -36,6 +43,25 @@ class TrinoFederatedQuerySeedTest {
                 .contains("queryContract")
                 .contains("postgres")
                 .contains("mysql");
+        assertThat(changelog)
+                .doesNotContain("connection-password=secret")
+                .doesNotContain("password\":\"secret")
+                .doesNotContain("root:");
+    }
+
+    @Test
+    void agentDatasourceChangelogShouldLinkFederatedGatewayToCopilotAiDatasource() throws Exception {
+        String changelog = readResource(FEDERATED_AGENT_DATASOURCE_CHANGELOG);
+
+        assertThat(changelog)
+                .contains("dbchangelog/dbchangelog-4.22.xsd")
+                .contains("copilot_ai.data_source")
+                .contains("联邦查询入口")
+                .contains("'trino'")
+                .contains("jdbc:trino://${DTS_TRINO_HOST:dts-trino}:${DTS_TRINO_PORT:8080}/postgres/public")
+                .contains("${DTS_TRINO_USER:copilot}")
+                .contains("copilotDataSourceId")
+                .contains("copilot_analytics.analytics_database");
         assertThat(changelog)
                 .doesNotContain("connection-password=secret")
                 .doesNotContain("password\":\"secret")
